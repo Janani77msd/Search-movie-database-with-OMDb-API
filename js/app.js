d63579ca331f9f2e9f172fd37ff2e886714daea2
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-07-14 21:46:04 -0400

    Updated AJAX callback

--- js/app.js
+++ js/app.js
@@ -6,27 +6,37 @@ var url = "http://www.omdbapi.com/?";
 var encodedtitle;
 var year;
 var data;
+var poster;
 
 // get results of id="search" (on keyup? or on submit?)
 $("#submit").on("click", function(event){
    console.log("Search submitted")
    event.preventDefault();
 // must clear html variable somewhere
+// must also clear poster images and titles
 // what happens if title or year is left blank? Display error messages?
 
    encodedtitle = encodeURIComponent($("#search").val());
    year = $("#year").val();
-   data = "s=" + encodedtitle + "&y=" + year + "&type=movie" + "&r=json";
+   //data = "s=" + encodedtitle + "&y=" + year + "&type=movie" + "&r=json";
+   data = {
+      s: encodedtitle,
+      y: year,
+      //type: movie,
+      r: "json"
+   }
 
-  $.getJSON(url, data, function(movie){
+  $.getJSON(url, data, function(response){
 
     html = "";
     // if there is no search data found (i.e., if (!movie.Title)), show relevant screen
     // else, cycle through response data
 
-       $.each(movie, function(){
-
-         if (!movie.Poster){
+       $.each(response.Search, function(index, movie){
+          console.log($(this));
+          console.log(movie.Title);
+         //if (!movie.Poster){
+         if (movie.Poster == "N/A"){
            poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
          } else {
            poster = "<img class='movie-poster' src=" + movie.Poster + ">";
@@ -43,7 +53,7 @@ $("#submit").on("click", function(event){
        });
 
     // append to ul
-    $("#movie").append(html);
+    $("#movies").append(html);
 
 
 }); // ends getJSON call
