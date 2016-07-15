c62c49edc47bb5f26026b28936acae75d94333d5
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-07-15 12:11:52 -0400

    Error screen added

--- js/app.js
+++ js/app.js
@@ -4,53 +4,55 @@
 var html;
 var url = "http://www.omdbapi.com/?";
 var encodedtitle;
+var title;
 var year;
 var data;
 var poster;
 
 // get results of id="search" (on keyup? or on submit?)
 $("#submit").on("click", function(event){
-   console.log("Search submitted")
    event.preventDefault();
-// must clear html variable somewhere
-// must also clear poster images and titles
-// what happens if title or year is left blank? Display error messages?
 
-   encodedtitle = encodeURIComponent($("#search").val());
+  // clear poster images and titles from previous searches
+  $("#movies").empty();
+
+  // what happens if title or year is left blank? Display error messages?
+
+   //encodedtitle = encodeURIComponent($("#search").val());
+   title = $("#search").val();
    year = $("#year").val();
-   //data = "s=" + encodedtitle + "&y=" + year + "&type=movie" + "&r=json";
    data = {
-      s: encodedtitle,
+      s: title,
       y: year,
-      //type: movie,
       r: "json"
    }
 
   $.getJSON(url, data, function(response){
 
     html = "";
-    // if there is no search data found (i.e., if (!movie.Title)), show relevant screen
-    // else, cycle through response data
+    // if there is no search data found, show error screen
+    if (response.Response == "False") {
+      html += "<li class='no-movies'>";
+      html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + $("#search").val() + "</li>";
+    } else {
+      // else, cycle through response data
+         $.each(response.Search, function(index, movie){
 
-       $.each(response.Search, function(index, movie){
-          console.log($(this));
-          console.log(movie.Title);
-         //if (!movie.Poster){
-         if (movie.Poster == "N/A"){
-           poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
-         } else {
-           poster = "<img class='movie-poster' src=" + movie.Poster + ">";
-         }
+           if (movie.Poster == "N/A"){
+             poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
+           } else {
+             poster = "<img class='movie-poster' src=" + movie.Poster + ">";
+           }
 
-         html += "<li>";
-         html += "<div class='poster-wrap'>";
-         html += poster;
-         html += "</div>";
-         html += "<span class='movie-title'>" + movie.Title + "</span>";
-         html += "<span class='movie-year'>" + movie.Year + "</span>";
-         html += "</li>";
-         // do I have to return html?
-       });
+           html += "<li>";
+           html += "<div class='poster-wrap'>";
+           html += poster;
+           html += "</div>";
+           html += "<span class='movie-title'>" + movie.Title + "</span>";
+           html += "<span class='movie-year'>" + movie.Year + "</span>";
+           html += "</li>";
+         }); // ends each
+    }
 
     // append to ul
     $("#movies").append(html);
