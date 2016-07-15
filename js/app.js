33bfcaece0ae40fa6d0cf3140e28fbfa97ebfb31
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-07-15 12:42:37 -0400

    Comments updated

--- js/app.js
+++ js/app.js
@@ -14,11 +14,11 @@ $("#submit").on("click", function(event){
    event.preventDefault();
 
   // clear poster images and titles from previous searches
-  $("#movies").empty();
+   $("#movies").empty();
 
   // what happens if title or year is left blank? Display error messages?
 
-   //encodedtitle = encodeURIComponent($("#search").val());
+   // encodedtitle = encodeURIComponent($("#search").val());
    title = $("#search").val();
    year = $("#year").val();
    data = {
@@ -32,26 +32,26 @@ $("#submit").on("click", function(event){
     html = "";
     // if there is no search data found, show error screen
     if (response.Response == "False") {
-      html += "<li class='no-movies'>";
-      html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + $("#search").val() + "</li>";
+       html += "<li class='no-movies'>";
+       html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + $("#search").val() + "</li>";
+    // else, cycle through response data
     } else {
-      // else, cycle through response data
-         $.each(response.Search, function(index, movie){
+       $.each(response.Search, function(index, movie){
 
-           if (movie.Poster == "N/A"){
-             poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
-           } else {
-             poster = "<img class='movie-poster' src=" + movie.Poster + ">";
-           }
+         if (movie.Poster == "N/A"){
+           poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
+         } else {
+           poster = "<img class='movie-poster' src=" + movie.Poster + ">";
+         }
 
-           html += "<li>";
-           html += "<div class='poster-wrap'>";
-           html += poster;
-           html += "</div>";
-           html += "<span class='movie-title'>" + movie.Title + "</span>";
-           html += "<span class='movie-year'>" + movie.Year + "</span>";
-           html += "</li>";
-         }); // ends each
+         html += "<li>";
+         html += "<div class='poster-wrap'>";
+         html += poster;
+         html += "</div>";
+         html += "<span class='movie-title'>" + movie.Title + "</span>";
+         html += "<span class='movie-year'>" + movie.Year + "</span>";
+         html += "</li>";
+       }); // ends each
     }
 
     // append to ul
