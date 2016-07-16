55624fc685065ab0578057c2be8965eda9cdf265
jprittie <Prittie@Jennifers-MacBook-Pro.local>
2016-07-16 15:20:09 -0400

    Pagination updated

--- js/app.js
+++ js/app.js
@@ -1,95 +1,132 @@
 // use strict
-// should this be in a document ready function?
+
+$(document).ready(function(){
+
+  // Initialize global variables
+  var totalListings;
+  var ajaxpagenumber;
+  var url;
+  var data;
 
 
-// Initialize global variables
-var totalListings;
+  // Once user submits search, clear any previous search results
+  // Then make ajax call using current search terms
+  $("#submit").on("click", function(event){
+    event.preventDefault();
 
-// Once user submits search, clear any previous search results
-// Then make ajax call using current search terms
-$("#submit").on("click", function(event){
-  event.preventDefault();
+    // Clear poster images and titles from previous searches
+    $("#movies").empty();
+    // And clear any existing pagination
+    $("#pagination").remove();
 
-  // Clear poster images and titles from previous searches
-  $("#movies").empty();
-  // And clear any existing pagination
-  $("#pagination").remove();
+    // Get current search terms
+    var title = $("#search").val();
+    var year = $("#year").val();
+    url = "http://www.omdbapi.com/?";
+    ajaxpagenumber = 1;
+    data = {
+       s: title,
+       y: year,
+       type: "movie",
+       r: "json",
+       page: ajaxpagenumber
+    };
 
-  // What happens if title or year is left blank? Display error messages?
+    // Make ajax call
+    $.getJSON(url, data, displayResults);
 
-  // Get current search terms
-  var title = $("#search").val();
-  var year = $("#year").val();
-  var url = "http://www.omdbapi.com/?";
-  pagenumber = 1;
-  var data = {
-     s: title,
-     y: year,
-     r: "json",
-     page: pagenumber
-  }
-
-  // Make ajax call
-  $.getJSON(url, data, displayResults);
+  }); // ends on click
 
 
+  // This function will insert search results into the html
+  function displayResults(response){
+    var html = "";
+    // If there is no search data found, show error screen
+    if (response.Response == "False") {
+       html += "<li class='no-movies'>";
+       html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + $("#search").val() + "</li>";
+    // Else, cycle through response data
+    } else {
+       $.each(response.Search, function(index, movie){
 
-}); // ends on click
+         // If there's no poster available, use placeholder
+         if (movie.Poster == "N/A"){
+           var poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
+         } else {
+           poster = "<img class='movie-poster' src=" + movie.Poster + ">";
+         }
 
-
-// This function will insert search results into the html
-function displayResults(response){
-  var html = "";
-  // If there is no search data found, show error screen
-  if (response.Response == "False") {
-     html += "<li class='no-movies'>";
-     html += "<i class='material-icons icon-help'>help_outline</i>No movies found that match: " + $("#search").val() + "</li>";
-  // Else, cycle through response data
-  } else {
-     $.each(response.Search, function(index, movie){
-
-       // If there's no poster available, use placeholder
-       if (movie.Poster == "N/A"){
-         var poster = "<i class='material-icons poster-placeholder'>crop_original</i>"
-       } else {
-         poster = "<img class='movie-poster' src=" + movie.Poster + ">";
-       }
-
-       // Build html to be appended
-       html += "<li>";
-       html += "<div class='poster-wrap'>";
-       html += poster;
-       html += "</div>";
-       html += "<span class='movie-title'>" + movie.Title + "</span>";
-       html += "<span class='movie-year'>" + movie.Year + "</span>";
-       html += "</li>";
-     }); // ends each
-  }
-  // Then append search results to ul
-  $("#movies").append(html);
+         // Build html to be appended
+         html += "<li>";
+         html += "<div class='poster-wrap'>";
+         html += poster;
+         html += "</div>";
+         html += "<span class='movie-title'>" + movie.Title + "</span>";
+         html += "<span class='movie-year'>" + movie.Year + "</span>";
+         html += "</li>";
+       }); // ends each
+    }
+    // Then append search results to ul
+    $("#movies").append(html);
 
 
-  // Get totalResults value from API, then call pagination function
-  var totalListings = response.totalResults;
-  console.log("There are " + totalListings + "total listings: " );
-  paginate();
-} // ends displayResults
+    // Get totalResults value from API, then call pagination function
+    totalListings = response.totalResults;
+    console.log("There are " + totalListings + " total listings: " );
+    paginate(totalListings);
+  } // ends displayResults
 
 
-/* The API has already paginated the results into groups of 10.
-   So, I fixed the CSS to display two rows of five across, because that's tidier
+  /* The API has already paginated the results into groups of 10.
+     So, I fixed the CSS to display two rows of five across, because that's tidier
+     Next, I will create function paginate(), which will calculate number of page links needed, and create them
+  */
 
-   Next, I will create function paginate(), which will:
-   - calculate number of page links needed, and create them
-   - when a link is clicked on, set pagenumber variable to the number of that link,
-   - then, make a new ajax call with updated pagenumber variable to get that page's results */
+  function paginate(){
+      // (Must round up or would get too few pages)
+      var pagesNeeded = Math.ceil(totalListings/10);
 
-function paginate(){
+      // Append a pagination div
+      $("body").append("<footer id='pagination'><ul id='paginationlist'></ul></footer>");
 
-  var pagesNeeded = totalListings/10;
-  (console.log pagesNeeded);
+      // Add the correct number of buttons to bottom of page, and set their contents and links.
+      for (var i=0; i< pagesNeeded; i++){
+        var newPageNumber = $("<li></li>");
+        newPageNumber.text(i+1);
+        $("#paginationlist").append(newPageNumber);
 
-  // Append a pagination div
-  $("#movies").append("<div id='pagination'><div>");
+      }
+   updateAjaxCall();
+   } // ends paginate
 
-}
+
+/* This function will load a new page of search results when a page number is clicked on.
+   To do that, it makes a new ajax call, in which the API's "page" property is set to the
+   number of the page that was clicked on.*/
+  function updateAjaxCall(){
+
+      $("#paginationlist li").click(function(){
+
+        // Clear poster images and titles from previous searches
+        $("#movies").empty();
+        // And clear any existing pagination
+        $("#pagination").remove();
+
+        // Update query string data
+        ajaxpagenumber = parseInt($(this).text());
+        var title = $("#search").val();
+        var year = $("#year").val();
+        url = "http://www.omdbapi.com/?";
+        data = {
+           s: title,
+           y: year,
+           type: "movie",
+           r: "json",
+           page: ajaxpagenumber
+        };
+        // Make new ajax call
+        $.getJSON(url, data, displayResults);
+      }); // ends anchor on click
+  } // ends updateAjaxCall
+
+}); // ends document ready
